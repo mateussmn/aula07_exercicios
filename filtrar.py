8be3b020b5aa49f5e9b985d777dfce517b0c979f
mateus <mateussilvamn18@gmail.com>
2026-06-29 14:00:40 -0300

    filtro de limite criado

--- filtrar.py
+++ filtrar.py
@@ -1,7 +1,15 @@
 #Filtrar Dados Acima de um Limite
+valores = [4,6,8,9,22,42]
+
 def filtrar_acima_de(valores: List[float], limite: float) -> List[float]:
     resultado = []
     for valor in valores:
         if valor > limite:
             resultado.append(valor)
-    return resultados
+    return resultado
+
+limite = float(input("Digite um valor limite: "))
+
+resultado = filtrar_acima_de(valores, limite)
+print(resultado)
+
